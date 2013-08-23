3d98eb81613cf6e6bfa9b0dd8d535933dbbb9845
RenaldasUrniezius <renaldasu@gmail.com>
2013-08-23 21:17:30 +0300

    C MrE Engine library for R
    R samples are provided : 1) One_at_One_constr.R - for Gaussian prior
    with one variance constraint on posterior; 2) SanovTheoremMrE.R - Adom's
    econometric MrE solution for Sanov's Theorem

---
+++ CLibraryMrEEngine/Preparation2.R
@@ -0,0 +1,10 @@
+library(Rcpp)
+library(inline)
+
+body <- 'NumericVector xx(x);return wrap( std::accumulate( xx.begin(), xx.end(), 0.0));'
+add <- cxxfunction(signature(x = "numeric"), body, plugin = "Rcpp")
+ 
+x <- 1
+y <- 2
+res <- add(c(x, y))
+res
